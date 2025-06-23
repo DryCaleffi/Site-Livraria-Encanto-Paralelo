a0c9a3c4be0897c32faa82dc65746996193c9ef5
Adryelle Calefi <149256629+DryCaleffi@users.noreply.github.com>
2025-06-23 12:52:20 -0300

    fazendo alterações, node funcionando, agora configurar o restante

--- livraria-encanto-paralelo/src/routes/pagesRoutes.ts
+++ livraria-encanto-paralelo/src/routes/pagesRoutes.ts
@@ -2,16 +2,16 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
-});
+//router.get('/', (req, res) => {
+//   res.render('index');
+//});
 
-router.get('/Aba_promocoes', (req, res) => {
-    res.render('pages/Aba_promocoes');
-});
+//router.get('/Aba_promocoes', (req, res) => {
+//    res.render('pages/Aba_promocoes');
+//});
 
-router.get('/Aba_Inicio', (req, res) => {
-    res.render('pages/Aba_Inicio');
-});
+//router.get('/Aba_Inicio', (req, res) => {
+//    res.render('pages/Aba_Inicio');
+//});
 
 export default router;
